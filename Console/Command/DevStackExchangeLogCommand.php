7752e7ea933065a777b7dc5be627982e4fbaab15
Tadeo Barranco <tbarrancoa@gmail.com>
2023-02-15 08:39:43 -0600

    [StackExchnage] - How To Add Arguments To A Custom CLI Command

--- Console/Command/DevStackExchangeLogCommand.php
+++ Console/Command/DevStackExchangeLogCommand.php
@@ -3,19 +3,34 @@
 namespace Barranco\StackExchange\Console\Command;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Magento\Framework\Console\Cli;
 
 class DevStackExchangeLogCommand extends Command
 {
+    public const ARGUMENT_LOG_LEVEL = 'level';
+
     /**
      * {@inheritdoc}
      */
     protected function configure()
     {
-        $this->setName('dev:stackexchange:log')
-            ->setDescription('Displays data using different log levels');
+        $this->setName('dev:stackexchange:log');
+        $this->setDescription('Displays data using different log levels');
+        $this->addArgument(
+            self::ARGUMENT_LOG_LEVEL,
+            InputArgument::REQUIRED,
+            'The log level to store data.'
+        );
+        $this->setHelp(
+            <<<HELP
+            To start logging data:
+
+            <comment>%command.full_name% someLevel</comment>
+            HELP
+        );
         
         parent::configure();
     }
@@ -25,7 +40,8 @@ class DevStackExchangeLogCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln('<info>Successfully log data</info>');
+        $logLevel = $input->getArgument(self::ARGUMENT_LOG_LEVEL);
+        $output->writeln('<info>Successfully ' . $logLevel . ' data logged</info>');
 
         return Cli::RETURN_SUCCESS;
     }
