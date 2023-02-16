ed527fe4ca97ab1e9cb263c9177a4cf95f91355e
Tadeo Barranco <tbarrancoa@gmail.com>
2023-02-16 08:13:13 -0600

    [StackExchnage] - Switch Case Based On Log Level

--- Console/Command/DevStackExchangeLogCommand.php
+++ Console/Command/DevStackExchangeLogCommand.php
@@ -41,8 +41,64 @@ class DevStackExchangeLogCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $logLevel = $input->getArgument(self::ARGUMENT_LOG_LEVEL);
-        $output->writeln('<info>Successfully ' . $logLevel . ' data logged</info>');
+
+        switch ($logLevel) {
+            case 'debug':
+                $this->writeSwitchCaseOutput($output, $logLevel);
+                break;
+
+            case 'info':
+                $this->writeSwitchCaseOutput($output, $logLevel);
+                break;
+
+            case 'notice':
+                $this->writeSwitchCaseOutput($output, $logLevel);
+                break;
+
+            case 'warning':
+                $this->writeSwitchCaseOutput($output, $logLevel);
+                break;
+
+            case 'error':
+                $this->writeSwitchCaseOutput($output, $logLevel);
+                break;
+
+            case 'critical':
+                $this->writeSwitchCaseOutput($output, $logLevel);
+                break;
+
+            case 'alert':
+                $this->writeSwitchCaseOutput($output, $logLevel);
+                break;
+
+            case 'emergency':
+                $this->writeSwitchCaseOutput($output, $logLevel);
+                break;
+
+            default:
+                $this->writeSwitchCaseOutput($output);
+                break;
+        }
+
 
         return Cli::RETURN_SUCCESS;
+    }
+
+    /**
+     * Write Output Switch Case Based
+     *
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     * @param string $logLevel
+     * @return bool
+     */
+    protected function writeSwitchCaseOutput(OutputInterface $output, $logLevel = '')
+    {
+        if ($logLevel !== '') {
+            $output->writeln('<info>Successfully ' . $logLevel . ' data logged</info>');
+        } else {
+            $output->writeln('<info>Non-existen log level');
+        }
+
+        return true;
     }
 }
